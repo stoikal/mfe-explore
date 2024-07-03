9f7c8a99e9198bb8bd13e54d1b4c98cd68180216
Laksito Pamilih <laksito@pm.me>
2024-07-03 16:56:34 +0700

    host: add antd

---
+++ host/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+import './App.css';
+
+function App () {
+  return (
+    <div>
+      <Button>button</Button>
+    </div>
+  );
+}
+
+export default App;
