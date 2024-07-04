0d94e658c4d01b2da2d11244f2afb8f12d1c659e
Laksito Pamilih <laksito@pm.me>
2024-07-04 16:21:47 +0700

    child-buku: change port

--- child-buku/src/index.js
+++ child-buku/src/index.js
@@ -2,14 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom";
 import App from "./App";
 
-window.mounthttplocalhost3001 = (containerId) => {
+window.mounthttplocalhost3002 = (containerId) => {
   ReactDOM.render(
     <App />,
     document.getElementById(containerId),
   );
 }
 
-window.unmounthttplocalhost3001 = (containerId) => {
+window.unmounthttplocalhost3002 = (containerId) => {
   return ReactDOM.unmountComponentAtNode(
     document.getElementById(containerId)
   );
